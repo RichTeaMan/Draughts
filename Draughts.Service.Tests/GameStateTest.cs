e600811d8b29709df8c0c0c4e5df3b4717d5a6e7
Thomas Holmes <thomasholmes@outlook.com>
2017-09-28 16:47:17 +0100

    Added basic move searching.

--- Draughts.Service.Tests/GameStateTest.cs
+++ Draughts.Service.Tests/GameStateTest.cs
@@ -7,6 +7,7 @@ namespace Draughts.Service.Tests
     [TestClass]
     public class GameStateTest
     {
+
         [TestMethod]
         public void SinglePieceGameState()
         {
@@ -23,6 +24,98 @@ namespace Draughts.Service.Tests
             Assert.AreEqual(pieceRank, gameState.GamePieceList.First().PieceRank);
             Assert.AreEqual(x, gameState.GamePieceList.First().Xcoord);
             Assert.AreEqual(y, gameState.GamePieceList.First().Ycoord);
+        }
+
+        [TestMethod]
+        public void SinglePieceWhiteGameMove()
+        {
+            var pieceColour = PieceColour.White;
+            var pieceRank = PieceRank.Minion;
+            int x = 2;
+            int y = 0;
+
+            var gamePiece = new GamePiece(pieceColour, pieceRank, x, y);
+            var gameState = GameStateFactory.SinglePieceGameState(gamePiece);
+
+            var moves = gameState.CalculateAvailableMoves();
+
+            var piece1 = moves[0].EndGamePiece;
+            var piece2 = moves[1].EndGamePiece;
+
+            Assert.AreEqual(2, moves.Count);
+
+            Assert.AreEqual(x - 1, piece1.Xcoord);
+            Assert.AreEqual(y + 1, piece1.Ycoord);
+
+            Assert.AreEqual(x + 1, piece2.Xcoord);
+            Assert.AreEqual(y + 1, piece2.Ycoord);
+        }
+
+        [TestMethod]
+        public void SinglePieceWhiteLeftEdgeGameMove()
+        {
+            var pieceColour = PieceColour.White;
+            var pieceRank = PieceRank.Minion;
+            int x = 0;
+            int y = 0;
+
+            var gamePiece = new GamePiece(pieceColour, pieceRank, x, y);
+            var gameState = GameStateFactory.SinglePieceGameState(gamePiece);
+
+            var moves = gameState.CalculateAvailableMoves();
+
+            var piece1 = moves[0].EndGamePiece;
+
+            Assert.AreEqual(1, moves.Count);
+
+            Assert.AreEqual(x + 1, piece1.Xcoord);
+            Assert.AreEqual(y + 1, piece1.Ycoord);
+        }
+
+        [TestMethod]
+        public void SinglePieceBlackGameMove()
+        {
+            var pieceColour = PieceColour.Black;
+            var pieceRank = PieceRank.Minion;
+            int x = 5;
+            int y = 7;
+
+            var gamePiece = new GamePiece(pieceColour, pieceRank, x, y);
+            var gameState = GameStateFactory.SinglePieceGameState(gamePiece);
+
+            var moves = gameState.CalculateAvailableMoves();
+
+            var piece1 = moves[0].EndGamePiece;
+            var piece2 = moves[1].EndGamePiece;
+
+            Assert.AreEqual(2, moves.Count);
+
+            Assert.AreEqual(x - 1, piece1.Xcoord);
+            Assert.AreEqual(y - 1, piece1.Ycoord);
+
+            Assert.AreEqual(x + 1, piece2.Xcoord);
+            Assert.AreEqual(y - 1, piece2.Ycoord);
+        }
+
+        [TestMethod]
+        public void SinglePieceBlackLeftEdgeGameMove()
+        {
+            var pieceColour = PieceColour.Black;
+            var pieceRank = PieceRank.Minion;
+            int x = 7;
+            int y = 7;
+
+            var gamePiece = new GamePiece(pieceColour, pieceRank, x, y);
+            var gameState = GameStateFactory.SinglePieceGameState(gamePiece);
+
+            var moves = gameState.CalculateAvailableMoves();
+
+            var piece1 = moves[0].EndGamePiece;
+
+            Assert.AreEqual(1, moves.Count);
+
+            Assert.AreEqual(x - 1, piece1.Xcoord);
+            Assert.AreEqual(y - 1, piece1.Ycoord);
         }
     }
 }
--- Draughts.Service/GameState.cs
+++ Draughts.Service/GameState.cs
@@ -21,9 +21,49 @@ namespace Draughts.Service
             YLength = yLength;
         }
 
-        public ICollection<GameMove> CalculateAvailableMoves()
+        public IList<GameMove> CalculateAvailableMoves()
         {
-            throw new NotImplementedException();
+            var gameMoves = new List<GameMove>();
+            foreach(var piece in GamePieceList)
+            {
+                int yDelta = 0;
+                if (piece.PieceColour == PieceColour.White)
+                {
+                    yDelta = 1;
+                } else if (piece.PieceColour == PieceColour.Black)
+                {
+                    yDelta = -1;
+                } else
+                {
+                    throw new ApplicationException("Unknown piece colour.");
+                }
+
+                int newY = piece.Ycoord + yDelta;
+
+                var newXcoords = new List<int>();
+                int newXleft = piece.Xcoord - 1;
+                if (newXleft >= 0)
+                {
+                    newXcoords.Add(newXleft);
+                }
+
+                int newXright = piece.Xcoord + 1;
+                if (newXright < XLength)
+                {
+                    newXcoords.Add(newXright);
+                }
+
+                foreach (var newX in newXcoords)
+                {
+                    var occupiedPiece = GamePieceList.SingleOrDefault(p => p.Xcoord == newX && piece.Ycoord == newY);
+                    if (occupiedPiece == null)
+                    {
+                        var endPiece = new GamePiece(piece.PieceColour, piece.PieceRank, newX, newY);
+                        gameMoves.Add(new GameMove(piece, endPiece, new List<GamePiece>()));
+                    }
+                }
+            }
+            return gameMoves;
         }
     }
 }
