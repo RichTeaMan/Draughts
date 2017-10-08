86ed51825d4c5c785019ea1afdb8682b4941875c
Thomas Holmes <thomasholmes@outlook.com>
2017-10-08 16:46:02 +0100

    Added checkerboard.

--- Draughts.UI.Wpf/DraughtsBoard.xaml.cs
+++ Draughts.UI.Wpf/DraughtsBoard.xaml.cs
@@ -20,9 +20,41 @@ namespace Draughts.UI.Wpf
     /// </summary>
     public partial class DraughtsBoard : UserControl
     {
+        public readonly int BoardWidth = 8;
+
+        public readonly int BoardHeight = 8;
+
+        public Brush DarkColourBrush { get; set; } = new SolidColorBrush(Color.FromRgb(140, 140, 140));
+
+        public Brush LightColourBrush { get; set; } = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
         public DraughtsBoard()
         {
             InitializeComponent();
+
+            foreach (var width in Enumerable.Range(0, BoardWidth))
+            {
+                foreach (var height in Enumerable.Range(0, BoardHeight))
+                {
+                    var cell = new Grid();
+                    if (width % 2 == 0 && height % 2 == 0)
+                    {
+                        cell.Background = LightColourBrush;
+                    }
+                    else if (width % 2 != 0 && height % 2 != 0)
+                    {
+                        cell.Background = LightColourBrush;
+                    }
+                    else
+                    {
+                        cell.Background = DarkColourBrush;
+                    }
+                    Grid.SetColumn(cell, width);
+                    Grid.SetRow(cell, height);
+                    Board.Children.Add(cell);
+                }
+            }
+
         }
     }
 }
