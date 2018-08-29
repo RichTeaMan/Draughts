a8db8206b1a1f420f449d1239a5d91a7107ec26c
Thomas Holmes <thomasholmes@outlook.com>
2018-08-29 23:08:02 +0100

    Changed turn limit to 150.

--- Draughts.Service/GameMatch.cs
+++ Draughts.Service/GameMatch.cs
@@ -13,7 +13,7 @@ namespace Draughts.Service
     {
         private List<GameState> _gameStateList = new List<GameState>();
 
-        public const int TurnLimit = 300;
+        public const int TurnLimit = 150;
 
         public GameState GameState { get; private set; }
 
