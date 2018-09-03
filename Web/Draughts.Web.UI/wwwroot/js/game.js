237043ccc3347f799175af5efa79ade24e8862c6
Thomas Holmes <thomasholmes@outlook.com>
2018-09-03 17:43:15 +0100

    Selected pieces now stay selected.

--- Web/Draughts.Web.UI/wwwroot/js/game.js
+++ Web/Draughts.Web.UI/wwwroot/js/game.js
@@ -74,7 +74,6 @@ function sendMove(startPiece, endX, endY) {
 
         console.log(data);
         renderBoard("game-grid", data);
-        currentGameBoard = data;
 
         $("td").removeClass("selected");
 
@@ -87,31 +86,37 @@ function reloadGame() {
     $.get(`/api/game?playerId=${playerId}`).done(function (data) {
         console.log(data);
         renderBoard("game-grid", data);
-        currentGameBoard = data;
     });
 }
 
 function renderBoard(destinationElementId, gameBoard) {
-    var html = '<table class="game-grid">';
 
-    for (var y = 0; y < gameBoard.height; y++) {
-        html += "<tr>";
-        for (var x = 0; x < gameBoard.width; x++) {
+    if (JSON.stringify(currentGameBoard) != JSON.stringify(gameBoard)) {
 
-            var transformedY = gameBoard.height - (y + 1);
-            html += `<td id="square-${x}-${transformedY}" data-x="${x}" data-y="${transformedY}" class="piece-square"><div></div></td>`;
+        var html = '<table class="game-grid">';
+
+        for (var y = 0; y < gameBoard.height; y++) {
+            html += "<tr>";
+            for (var x = 0; x < gameBoard.width; x++) {
+
+                var transformedY = gameBoard.height - (y + 1);
+                html += `<td id="square-${x}-${transformedY}" data-x="${x}" data-y="${transformedY}" class="piece-square"><div></div></td>`;
+            }
+            html += "</tr>";
         }
-        html += "</tr>";
-    }
-    html += "</table>";
+        html += "</table>";
 
-    $(`#${destinationElementId}`).html(html);
+        $(`#${destinationElementId}`).html(html);
 
-    for (var i = 0; i < gameBoard.gamePieces.length; i++) {
+        for (var i = 0; i < gameBoard.gamePieces.length; i++) {
 
-        var piece = gameBoard.gamePieces[i];
-        var id = `#square-${piece.xcoord}-${piece.ycoord}`;
+            var piece = gameBoard.gamePieces[i];
+            var id = `#square-${piece.xcoord}-${piece.ycoord}`;
 
-        $(id).addClass(piece.pieceColour);
+            $(id).addClass(piece.pieceColour);
+        }
+
+
+        currentGameBoard = gameBoard;
     }
 }
